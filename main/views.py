c4c9438193ac45d7e10c5e8a8e1660d96b3076fa
Jacky56 <jackybanh1997@gmail.com>
2020-04-21 01:36:44 +0100

    add template (improper template)

--- main/views.py
+++ main/views.py
@@ -1,26 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.sites.shortcuts import get_current_site
+from . import template
 # Create your views here.
 
+
+
+
+
+
 def homepage(request):
-	word =  'http://' + get_current_site(request).domain
 	return HttpResponse("""
+	{}
 	<h1>hello world</h1>
 	<div>yeet</div>
 	<div>yeet</div>
 	<h2>this is home</h2>
-	<div>{}</div>
-	<button onclick="window.location.href = 'http://127.0.0.1:8000/ye';">ye?</button>
-	""".format(word, word))
+	""".format(template.template1(request)))
 
 	
 def ye(request):
-	word =  'http://' + get_current_site(request).domain
 	return HttpResponse("""
+	{}
 	<h1>hye</h1>
 	<div>yeet</div>
 	<div>yeet</div>
 	<h2>this is ye</h2>
-	<button onclick="window.location.href = '{}';">home?</button>
-	""".format(word))
+	""".format(template.template1(request)))
+
